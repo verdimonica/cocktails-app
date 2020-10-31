26d9f3d77cd27c075f635b4440ef1ed75eba9a6c
Monica Verdi <monica.verdi@gmail.com>
2020-10-31 12:49:02 +0100

    responsive burguer menu

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// Opening and closing menu on burguer click
+var openMenu = function(){
+    el = document.getElementsByTagName("aside")[0];
+    el.style.display = "block";
+    document.getElementById("burguer-menu").style.display = "none";
+}
+
+var closeMenu = function(){
+    el = document.getElementsByTagName("aside")[0];
+    el.style.display = "none";
+    document.getElementById("burguer-menu").style.display = "inline";
+}
+
+document.getElementById("burguer-menu").addEventListener("click", openMenu);
+document.getElementById("aside-logo-mb").addEventListener("click", closeMenu);
