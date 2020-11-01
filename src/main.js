8590d00dba83acbaa32dc5d2762a7dad3b941962
Monica Verdi <monica.verdi@gmail.com>
2020-11-01 15:50:27 +0100

    css buttons

--- src/main.js
+++ src/main.js
@@ -16,28 +16,5 @@ document.getElementById("aside-logo-mb").addEventListener("click", closeMenu);
 
 
 //faq section
-@import url(https://fonts.googleapis.com/css?family=Roboto:400,300,500);
-$(".open").click( function () {
-    var container = $(this).parents(".topic");
-    var answer = container.find(".answer");
-    var trigger = container.find(".faq-t");
-    
-    answer.slideToggle(200);
-    
-    if (trigger.hasClass("faq-o")) {
-      trigger.removeClass("faq-o");
-    }
-    else {
-      trigger.addClass("faq-o");
-    }
-    
-    if (container.hasClass("expanded")) {
-      container.removeClass("expanded");
-    }
-    else {
-      container.addClass("expanded");
-    }
-  });
-
 
   //search page
